eb74488e5137dbb68d252fc7c9cd4fd447c9f1a6
Opilane TTHK <opilane@tthk.ee>
2022-01-18 13:30:52 +0200

    Add project files.

---
+++ Bilerid.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinuVorm
+{
+    class Bilerid : Form
+    {
+        Button btne_sal_2;
+        Button btne_sal_1;
+        Button btne_sal_3;
+        public Bilerid()
+            {
+            this.ClientSize = new System.Drawing.Size(390, 200);
+            BackColor = Color.LightGray;
+            this.Text = "Saali valik";
+            Label message = new Label();
+            Button[] btn = new Button[4];
+            string[] texts = new string[4];
+            TableLayoutPanel tlp = new TableLayoutPanel();
+            Button btn_tabel;
+            int btn_w, btn_h;
+
+            btne_sal_1 = new Button();
+            btne_sal_1.Text = "12 / 12";
+            btne_sal_1.Location = new Point(250, 60);
+            btne_sal_1.Height = 40;
+            btne_sal_1.Width = 100;
+            btne_sal_1.BackColor = Color.DarkGray;
+            btne_sal_1.Font = new Font("Times New Roman", 15.0f);
+            btne_sal_1.Click += Start_btn_2_Click1;
+
+
+            btne_sal_2 = new Button();
+            btne_sal_2.Text = "9 / 9";
+            btne_sal_2.Location = new Point(145, 60);
+            btne_sal_2.Height = 40;
+            btne_sal_2.Width = 100;
+            btne_sal_2.BackColor = Color.DarkGray;
+            btne_sal_2.Font = new Font("Times New Roman", 15.0f);
+            btne_sal_2.Click += btne_Click;
+
+
+            btne_sal_3 = new Button();
+            btne_sal_3.Text = "6 / 6";
+            btne_sal_3.Location = new Point(40, 60);
+            btne_sal_3.Height = 40;
+            btne_sal_3.Width = 100;
+            btne_sal_3.BackColor = Color.DarkGray;
+            btne_sal_3.Font = new Font("Times New Roman", 15.0f);
+            btne_sal_3.Click += Btne_sal_3_Click;
+
+
+
+            this.Controls.Add(btne_sal_1);
+            this.Controls.Add(btne_sal_2);
+            this.Controls.Add(btne_sal_3);
+
+
+        }
+
+        private void Btne_sal_3_Click(object sender, EventArgs e)
+        {
+            MyForm uus_aken = new MyForm(6, 6);
+            uus_aken.StartPosition = FormStartPosition.CenterScreen;
+            uus_aken.ShowDialog();
+        }
+
+        private void Start_btn_2_Click1(object sender, EventArgs e)
+        {
+            MyForm uus_aken = new MyForm(12, 12);
+            uus_aken.StartPosition = FormStartPosition.CenterScreen;
+            uus_aken.ShowDialog();
+        }
+
+        private void btne_Click(object sender, EventArgs e)                      //кол-во кнопок
+        {
+            MyForm uus_aken = new MyForm(9, 9);
+            uus_aken.StartPosition = FormStartPosition.CenterScreen;
+            uus_aken.ShowDialog();
+        }
+    }
+}
---
+++ KinoForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinuVorm
+{
+
+    public partial class KinoForm : Form
+    {
+
+        public PictureBox img = new PictureBox();
+        private Control image;
+
+        public Button but;
+        private Control button;
+
+
+        public KinoForm()
+        {
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            BackColor = Color.LightGray;
+            this.Text = "Meil on hea meel teid näha!";
+
+
+            TextBox box = new TextBox();
+            box.Location = new Point(260, 100);
+
+            box.Text = "Tere, Tere tulemast kinno \"Lootus\"";
+            Font myfont = new Font("Times New Roman", 25.0f);        //изменения размера шрифта
+            box.Font = myfont;                                    // хз
+
+            box.ReadOnly = true;                                //только для чтения
+            box.Height = 150;
+            box.Width = 460;
+            box.BackColor = Color.LightGray;                //изменяет забний цвет на цвет страницы
+            box.BorderStyle = 0;                             // уберает чёрные рамки
+
+            this.Controls.Add(box);
+            but = new Button();
+            but.BackColor = Color.LightGray;
+            but.Text = "Pileti ostmiseks";
+            but.Size = new Size(120, 70);
+            but.ForeColor = Color.Black;
+            but.Location = new Point(400, 375);
+            but.Font = new Font("Times New Roman", 18);
+            but.Click += new EventHandler(but_Click);
+            this.Controls.Add(but);
+
+            //////////////////////////////////////////////////////////////////////////// 
+            img.Size = new Size(1000, 600);
+            img.Image = Image.FromFile(@"..\..\img\kinoshka2.jpg");
+            this.Controls.Add(img);
+            /////////////////////////////////////////////////////////////////////////////////////
+            //
+        }
+
+        private void but_Click(object sender, EventArgs e)
+        {
+            Bilerid uus_aken = new Bilerid();
+            this.Hide();
+            uus_aken.StartPosition = FormStartPosition.CenterScreen;
+            uus_aken.ShowDialog();
+        }
+    }
+}
